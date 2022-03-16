903848f409df7572f73be36fcbb11b6802edf8ca
natnael-OQP <makiya112545@gmail.com>
2022-03-16 21:51:16 +0300

    create header

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Header() {
+  return (
+    <View>
+      <Text>grab your</Text>
+      <Text>Delicious meal</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
