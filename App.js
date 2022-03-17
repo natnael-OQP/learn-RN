6d3b57801aacc483378c9444a746460c54a9de78
natnael-OQP <makiya112545@gmail.com>
2022-03-17 22:09:14 +0300

    add search component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
+import Search from "./components/Search";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <Search />
       <StatusBar style="auto" />
     </View>
   );
