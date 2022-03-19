cf15f8a26be852c5c5e9ed1cd7bde6c26f812fcd
natnael-OQP <makiya112545@gmail.com>
2022-03-19 22:00:26 +0300

    change term

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, TextInput } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import { elevation } from "./common/style";
 
-export default function Search() {
+export default function Search({ setTerm }) {
+  const [input, setInput] = useState();
+  const handleEnter = () => {
+    if (!input) return;
+    setTerm(input);
+  };
   return (
     <View style={[styles.container, styles.elevation]}>
       <FontAwesome name="search" size={25} />
-      <TextInput placeholder="Restaurant, food" style={styles.input} />
+      <TextInput
+        placeholder="Restaurant, food"
+        style={styles.input}
+        onChangeText={(text) => setInput(text)}
+        onEndEditing={() => handleEnter()}
+      />
     </View>
   );
 }
