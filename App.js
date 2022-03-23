fa3c60a68f8e679a2ecb669a1f562635d08a8d3c
natnael-OQP <makiya112545@gmail.com>
2022-03-23 22:04:37 +0300

    screens created

--- App.js
+++ App.js
@@ -1,51 +1,19 @@
-import { useState, useEffect } from "react";
-import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
-import Header from "./src/components/Header";
-import Search from "./src/components/Search";
-import Category from "./src/components/Category";
-import Restaurant from "./src/components/Restaurant";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreens from "./src/screens/HomeScreens";
+import ProductScreens from "./src/screens/ProductScreens";
 
-export default function App() {
-  const [term, setTerm] = useState("watch");
-  // term
-  const commonCategories = [
-    {
-      name: "watch",
-      img: require("./src/assets/images/pizza.png"),
-    },
-    {
-      name: "hats",
-      img: require("./src/assets/images/cake.png"),
-    },
-    {
-      name: "sneakers",
-      img: require("./src/assets/images/smoothies.png"),
-    },
-    {
-      name: "jackets",
-      img: require("./src/assets/images/steak.png"),
-    },
-    {
-      name: "womens",
-      img: require("./src/assets/images/pasta.png"),
-    },
-    {
-      name: "mens",
-      img: require("./src/assets/images/pasta.png"),
-    },
-  ];
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View>
-      <Header />
-      <Search setTerm={setTerm} />
-      <Category
-        term={term}
-        commonCategories={commonCategories}
-        setTerm={setTerm}
-      />
-      <Restaurant term={term} />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreens} />
+        <Stack.Screen name="Details" component={ProductScreens} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
